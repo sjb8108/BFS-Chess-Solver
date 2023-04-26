520273e8da23001f863b62448486d57c7c9c6eaa
Maanav Contractor (mpc9618) <mpc9618@rit.edu>
2023-04-26 10:38:20 -0400

    Hoppers puzzle and HoppersConfig both implemented, and the Hoppers puzzle matches all the given test files

--- src/puzzles/hoppers/model/HoppersConfig.java
+++ src/puzzles/hoppers/model/HoppersConfig.java
@@ -3,21 +3,174 @@ package puzzles.hoppers.model;
 import puzzles.common.solver.Configuration;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.util.*;
 
 // TODO: implement your HoppersConfig for the common solver
 
 public class HoppersConfig implements Configuration{
-    public HoppersConfig(String filename) throws IOException {
+    private String[][] hopperBoard;
+    private static int rowDim;
+    private static int colDim;
+    public HoppersConfig(String[][] hopperBoard) {
+        this.hopperBoard = hopperBoard;
+        rowDim = hopperBoard.length;
+        colDim = hopperBoard[0].length;
     }
 
     @Override
     public boolean isSolution() {
+        int redFrogCount = 0;
+        int greenFrogCount = 0;
+        for (int iRow = 0; iRow < rowDim; iRow++) {
+            for (int iCol = 0; iCol < colDim; iCol++) {
+                if (!(this.hopperBoard[iRow][iCol].equals(".") &&
+                        (this.hopperBoard[iRow][iCol].equals("*")))) {
+                    if (this.hopperBoard[iRow][iCol].equals("R")) {
+                        redFrogCount++;
+                    } else if (this.hopperBoard[iRow][iCol].equals("G")) {
+                        greenFrogCount++;
+                    }
+                }
+            }
+        }
+        if (greenFrogCount == 0 && redFrogCount == 1) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public Collection<Configuration> getNeighbors() {
-        return null;
+        Set<Configuration> neighbors = new LinkedHashSet<Configuration>();
+        for (int iRow = 0; iRow < rowDim; iRow++) {
+            for (int iCol = 0; iCol < colDim; iCol++) {
+                if (this.hopperBoard[iRow][iCol].equals("G") ||
+                        this.hopperBoard[iRow][iCol].equals("R")) {
+
+                    String frog = this.hopperBoard[iRow][iCol];
+
+                    // Try jumping North
+                    try {
+                        if (this.hopperBoard[iRow-2][iCol].equals("G") && this.hopperBoard[iRow-4][iCol].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow-4,
+                                    iCol, iRow-2, iCol, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+
+                    // Try jumping North East
+                    try {
+                        if (this.hopperBoard[iRow-1][iCol+1].equals("G") && this.hopperBoard[iRow-2][iCol+2].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow-2,
+                                    iCol+2, iRow-1, iCol+1, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+
+                    // Try jumping East
+                    try {
+                        if (this.hopperBoard[iRow][iCol+2].equals("G") && this.hopperBoard[iRow][iCol+4].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow,
+                                    iCol+4, iRow, iCol+2, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+
+                    // Try jumping South East
+                    try {
+                        if (this.hopperBoard[iRow+1][iCol+1].equals("G") && this.hopperBoard[iRow+2][iCol+2].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow+2,
+                                    iCol+2, iRow+1, iCol+1, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+
+                    // Try jumping South
+                    try {
+                        if (this.hopperBoard[iRow+2][iCol].equals("G") && this.hopperBoard[iRow+4][iCol].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow+4,
+                                    iCol, iRow+2, iCol, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+
+                    // Try jumping South West
+                    try {
+                        if (this.hopperBoard[iRow+1][iCol-1].equals("G") && this.hopperBoard[iRow+2][iCol-2].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow+2,
+                                    iCol-2, iRow+1, iCol-1, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+
+                    // Try jumping West
+                    try {
+                        if (this.hopperBoard[iRow][iCol-2].equals("G") && this.hopperBoard[iRow][iCol-4].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow,
+                                    iCol-4, iRow, iCol-2, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+
+                    // Try jumping North West
+                    try {
+                        if (this.hopperBoard[iRow-1][iCol-1].equals("G") && this.hopperBoard[iRow-2][iCol-2].equals(".")) {
+                            neighbors.add(makeNeighbor(iRow-2,
+                                    iCol-2, iRow-1, iCol-1, iRow, iCol, frog));
+                        }
+                    } catch (IndexOutOfBoundsException ie) {}
+                }
+            }
+        }
+        return neighbors;
     }
+
+
+    public HoppersConfig makeNeighbor(int newRow, int newCol, int removeRow, int removeCol,
+                                      int originalRow, int originalCol, String frog) throws IndexOutOfBoundsException {
+        String[][] newHopperBoard = new String[rowDim][colDim];
+        for (int iRow = 0; iRow < rowDim; iRow++) {
+            for (int iCol = 0; iCol < colDim; iCol++) {
+                if (iRow == newRow && iCol == newCol) {
+                    newHopperBoard[iRow][iCol] = frog;
+                } else if (iRow == removeRow && iCol == removeCol) {
+                    newHopperBoard[iRow][iCol] = ".";
+                } else if (iRow == originalRow && iCol == originalCol) {
+                    newHopperBoard[iRow][iCol] = ".";
+                } else {
+                    newHopperBoard[iRow][iCol] = this.hopperBoard[iRow][iCol];
+                }
+            }
+        }
+        return new HoppersConfig(newHopperBoard);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof HoppersConfig otherHoppers) {
+            for (int i = 0; i < rowDim; i++) {
+                for (int j = 0; j < colDim; j++) {
+                    if (!(this.hopperBoard[i][j].equals(otherHoppers.hopperBoard[i][j]))) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(hopperBoard);
+    }
+
+    @Override
+    public String toString() {
+        String finalString = "";
+        for (int iRow = 0; iRow < rowDim; iRow++) {
+            String rowString = "";
+            for (int iCol = 0; iCol < colDim; iCol++) {
+                rowString += hopperBoard[iRow][iCol] + " ";
+            }
+            finalString += rowString + "\n";
+        }
+        return finalString;
+    }
+
 }
+
+
--- src/puzzles/hoppers/solver/Hoppers.java
+++ src/puzzles/hoppers/solver/Hoppers.java
@@ -1,11 +1,40 @@
 package puzzles.hoppers.solver;
 
 import puzzles.common.solver.Solver;
+import puzzles.hoppers.model.HoppersConfig;
+
+import java.io.*;
 
 public class Hoppers {
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java Hoppers filename");
-        }
+    public static void main(String[] args) throws IOException {
+        try {
+            if (args.length != 1) {
+                System.out.println("Usage: java Hoppers filename");
+            } else {
+                BufferedReader hopperLoader = new BufferedReader( new FileReader(args[0]));
+                String[] dimensions = hopperLoader.readLine().split(" ");
+                int rowDim = Integer.parseInt(dimensions[0]);
+                int colDim = Integer.parseInt(dimensions[1]);
+                String[][] hopperBoard = new String[rowDim][colDim];
+
+                for (int iRow = 0; iRow < rowDim; iRow++) {
+                    String[] line = hopperLoader.readLine().split("\\s+");
+                    for (int iCol = 0; iCol < colDim; iCol++) {
+                        hopperBoard[iRow][iCol] = line[iCol];
+                    }
+                }
+
+                System.out.println("File: " + args[0]);
+                for (int iRow = 0; iRow < rowDim; iRow++) {
+                    for (int iCol = 0; iCol < colDim; iCol++) {
+                        System.out.print(hopperBoard[iRow][iCol] + " ");
+                    }
+                    System.out.println();
+                }
+                HoppersConfig initialConfig = new HoppersConfig(hopperBoard);
+                Solver.solve(initialConfig);
+
+            }
+        } catch (Exception e) {System.out.println("Invalid file exception thrown");}
     }
 }
